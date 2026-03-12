7a5f113f68e3adcfc584cd526267780905a54a56
AlexandreSaito <75994293+AlexandreSaito@users.noreply.github.com>
2026-03-12 13:41:52 -0300

    moved resume command to his own file

--- commands/pause.go
+++ commands/pause.go
@@ -16,16 +16,3 @@ func (c *PauseCommand) Execute(p domain.PerfilInterface, args []string) error {
 }
 
 var _ Command = (*PauseCommand)(nil)
-
-type ResumeCommand struct{}
-
-func (c *ResumeCommand) Name() string        { return "resume" }
-func (c *ResumeCommand) Description() string { return "Resume paused track" }
-
-func (c *ResumeCommand) Execute(p domain.PerfilInterface, args []string) error {
-	p.Player().Resume()
-	p.Output().Display("Resumed")
-	return nil
-}
-
-var _ Command = (*ResumeCommand)(nil)
---
+++ commands/resume.go
@@ -0,0 +1,18 @@
+package commands
+
+import (
+	"tocadormusica/domain"
+)
+
+type ResumeCommand struct{}
+
+func (c *ResumeCommand) Name() string        { return "resume" }
+func (c *ResumeCommand) Description() string { return "Resume paused track" }
+
+func (c *ResumeCommand) Execute(p domain.PerfilInterface, args []string) error {
+	p.Player().Resume()
+	p.Output().Display("Resumed")
+	return nil
+}
+
+var _ Command = (*ResumeCommand)(nil)
